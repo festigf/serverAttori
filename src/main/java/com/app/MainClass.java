b737ccaea93c039cba8d24fb80482ccb5a29a5e8
festigf <gianfranco.festi@marconirovereto.it>
2018-03-23 16:29:28 +0100

    third

--- src/main/java/com/app/MainClass.java
+++ src/main/java/com/app/MainClass.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.*;
 @SpringBootApplication
 public class MainClass {
 	public static void main(String[] args)  {
+		//test changes  s
 		SpringApplication.run(MainClass.class,args);
 	}
 }
